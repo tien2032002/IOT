0106a9bd7d0f3fb5be948aedd9ebe06c31d378f6
tien2032002 <80495035+tien2032002@users.noreply.github.com>
2024-02-19 21:58:10 +0700

    update code

--- lab2.py
+++ lab2.py
@@ -2,10 +2,11 @@ import sys
 from Adafruit_IO import MQTTClient
 import random
 import time
+from simpleAI import *
 
 AIO_FEED_IDs = ["button1"]
 AIO_USERNAME = "tien2032002"
-AIO_KEY = "aio_jczR58scqGob33Af3L6O8echPdkF"
+AIO_KEY = "aio_ujXn77sn3RraI7wP8xWMOh4OCdu4"
 MODEL_PATH = "IOT\keras_model.h5"
 CLASS_NAME_PATH = "IOT\labels.txt"
 
@@ -32,9 +33,20 @@ client.on_subscribe = subscribe
 client.connect()
 client.loop_background()
 
+# Disable scientific notation for clarity
+np.set_printoptions(suppress=True)
 
+# Load the model
+model = load_model("keras_Model.h5", compile=False)
+
+# Load the labels
+class_names = open("labels.txt", "r").readlines()
+
+# CAMERA can be 0 or 1 based on default camera of your computer
+camera = cv2.VideoCapture(0)
 
 while True:
     client.publish("button1", random.randint(0, 1))
+    client.publish("AI", simpleAI(model, class_names))
     time.sleep(5)
     pass
